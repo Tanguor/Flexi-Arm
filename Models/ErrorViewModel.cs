19cdb3510532ccff5556288d5a4303948cf88441
Tanguy LEBRET <tanguy.lebret@vinci-energies.net>
2023-03-24 16:27:26 +0100

    Commentaire des éléments modèle du site

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -7,3 +7,7 @@ namespace Flexi_Arm.Models
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
+
+// Ce fichier contient la définition de la classe ErrorViewModel utilisée pour afficher des erreurs dans les vues.
+// La classe a une propriété RequestId qui peut être définie pour afficher des informations sur une demande d'erreur spécifique,
+// et une méthode ShowRequestId pour déterminer si une RequestId est définie.
--- Models/Recette.cs
+++ Models/Recette.cs
@@ -4,13 +4,12 @@ namespace Flexi_Arm.Models;
 
 public class Recette
 {
-    [Key]
+    [Key] // Définit la clé primaire de la table
     public int Id { get; set; }
-
-    [Required]
-    [Display(Name = "Nom")]
-    [StringLength(50)]
+    [Required] // Indique que la propriété est obligatoire
+    [Display(Name = "Nom")] // Définit le nom à afficher dans l'interface utilisateur
+    [StringLength(50)] // Définit la longueur maximale de la chaîne
     public string Name { get; set; }
-    public string Description { get; set; }
 
+    public string Description { get; set; } // Description de la recette
 }
